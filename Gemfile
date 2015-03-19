c92fc532ab44604d9634e51e4fdac0e06778fc01
Aleksandar Tokarev <a.tokarev@t-home.mk>
2015-03-19 20:58:28 +0000

    Add breadcrumbs page navigation

--- Gemfile
+++ Gemfile
@@ -19,6 +19,7 @@ gem 'carrierwave'
 gem "rmagick"
 gem 'wicked_pdf'
 gem 'wkhtmltopdf-binary'
+gem "breadcrumbs_on_rails"
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
 
   after_filter :set_csrf_cookie_for_ng
+  
+  add_breadcrumb "Home", :root_path
 
   def set_csrf_cookie_for_ng
     cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
